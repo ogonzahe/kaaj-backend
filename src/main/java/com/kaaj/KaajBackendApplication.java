2f004c3d6c9aeaaa1a2be88f93acbc6e53d81420
ogonzahe <cgonzahe@gmail.com>
2025-09-24 19:21:32 -0600

    Initialize Spring Boot project with Maven wrapper, basic application structure, and date API endpoint

---
+++ src/main/java/com/kaaj/KaajBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kaaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaajBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaajBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaaj/kaaj_backend/KaajBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaaj.kaaj_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaajBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
